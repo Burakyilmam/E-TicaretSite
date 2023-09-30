76d77df8453b72d4d629920a28220ae638b621c7
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2023-09-30 17:27:40 +0300

    Userpage içerisine hava durumunu ve kullanıcı yorum sayısını getirdim.

---
+++ E-TicaretSite.Web/ViewComponents/UserComments.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Context;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_TicaretSite.Web.ViewComponents
+{
+    public class UserComments : ViewComponent
+    {
+        DataContext c = new DataContext();
+        public IViewComponentResult Invoke()
+        {
+            var user = c.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+            ViewBag.Username = User.Identity.Name;
+            ViewBag.Id = user.Id;
+            var comments = c.Comments.Where(x => x.UserId == user.Id && x.Statu == true).Count();
+            ViewBag.comments = comments;
+            return View();
+        }
+    }
+}
---
+++ E-TicaretSite.Web/ViewComponents/Weather.cs
@@ -0,0 +1,63 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Xml.Linq;
+
+//namespace E_TicaretSite.Web.ViewComponents
+//{
+//    public class Weather : ViewComponent
+//    {
+//        public IViewComponentResult Invoke()
+//        {
+//            string api = "0928835605ea3e6b03f3a11f83b9d751";
+//            string city = "Bursa";
+//            string connection = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}";
+//            return View();
+//        }
+//    }
+//}
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+public class Weather : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        string api = "0928835605ea3e6b03f3a11f83b9d751";
+        string city = "Bursa";
+        string baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"{baseUrl}?q={city}&appid={api}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+                    double kelvinTemperature = json["main"]["temp"].Value<double>();
+                    double celsiusTemperature = kelvinTemperature - 273.15;
+                    string iconCode = json["weather"][0]["icon"].Value<string>();
+                    string iconUrl = $"http://openweathermap.org/img/w/{iconCode}.png";
+                    ViewBag.IconUrl = iconUrl;
+                    ViewBag.Temperature = celsiusTemperature.ToString("0");
+                    ViewBag.City = json["name"];
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        return View();
+    }
+}
