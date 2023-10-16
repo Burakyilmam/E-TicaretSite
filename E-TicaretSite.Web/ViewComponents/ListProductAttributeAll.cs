5b297c07d75bab3246f216622e40fc4670e5684d
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2023-10-16 17:12:25 +0300

    Ürün sayfasına ürün özellikleri eklendi.

---
+++ E-TicaretSite.Web/ViewComponents/ListProductAttributeAll.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_TicaretSite.Web.ViewComponents
+{
+    public class ListProductAttributeAll : ViewComponent
+    {
+        ProductAttributeManager pam = new ProductAttributeManager((new EfProductAttributeRepository()));
+        public IViewComponentResult Invoke(int id)
+        {
+            var value = pam.ListProductAttributeWithCategory(id);
+            return View(value);
+        }
+    }
+}
